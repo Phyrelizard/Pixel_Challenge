c006e9fc22bf7d7739961b59e200e65e79e01d22
phyrelizard <ds.smith916@gmail.com>
2026-04-07 00:50:56 -0700

    v22.16.1 - added thrid audio volume for voice.  check in and confirm in 1 line.

--- start_console.sh
+++ start_console.sh
@@ -52,7 +52,7 @@ cd /home/ledgame/easter_game
 #python /home/ledgame/easter_game/pixel_challenge_console_v22.9.0.py 
 #python /home/ledgame/easter_game/pixel_challenge_console_v22.10.1.py 
 #python /home/ledgame/easter_game/pixel_challenge_console_v22.12.0.py
-python /home/ledgame/easter_game/pixel_challenge_console_v22.14.1.py
+python /home/ledgame/easter_game/pixel_challenge_console_v22.16.1.py
 
 
 
