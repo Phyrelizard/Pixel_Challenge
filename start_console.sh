45642ecac5285b6ba752801490c6b9769e223bef
phyrelizard <ds.smith916@gmail.com>
2026-04-10 15:03:35 -0700

    v25.4.0 - improving dmx control gui section

--- start_console.sh
+++ start_console.sh
@@ -53,7 +53,7 @@ cd /home/ledgame/easter_game
 #python /home/ledgame/easter_game/pixel_challenge_console_v22.10.1.py 
 #python /home/ledgame/easter_game/pixel_challenge_console_v22.12.0.py
 #python /home/ledgame/easter_game/pixel_challenge_console_v22.16.1.py 
-python /home/ledgame/easter_game/pixel_challenge_console_v25.3.4.py
+python /home/ledgame/easter_game/pixel_challenge_console_v25.4.0.py
 
 
 
