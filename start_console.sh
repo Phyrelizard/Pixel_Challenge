11cb91a1777c8071687f040d5bb9c0b517b7b90e
phyrelizard <ds.smith916@gmail.com>
2026-04-15 11:15:13 -0700

    v27.1.0 - implemented new editor called visulizer, but locks up when launched.

--- start_console.sh
+++ start_console.sh
@@ -53,7 +53,7 @@ cd /home/ledgame/easter_game
 #python /home/ledgame/easter_game/pixel_challenge_console_v22.10.1.py 
 #python /home/ledgame/easter_game/pixel_challenge_console_v22.12.0.py
 #python /home/ledgame/easter_game/pixel_challenge_console_v22.16.1.py 
-python /home/ledgame/easter_game/pixel_challenge_console_v26.7.0.py
+python /home/ledgame/easter_game/pixel_challenge_console_v27.1.0.py
 
 
 
